55ba35f978673f8a436e562dda27482840363a14
Parnell Steide <parnellsteide@Parnells-Air.(none)>
2023-04-10 19:23:19 -0400

    Connect app to local api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SnakeCardComponent } from './components/snake-card/snake-card.component';
+import { SnakeCardContainerComponent } from './components/snake-card-container/snake-card-container.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SnakeCardComponent, SnakeCardContainerComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
